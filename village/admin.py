bb886c064157079ebf88dda38df0a7d387828527
UmuhireAnuarithe <you@example.com>
2019-11-05 19:01:09 +0200

    registered models in admin.py

--- village/admin.py
+++ village/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Profile,Business,Village,Profile
 # Register your models here.
+admin.site.register(Events)
+admin.site.register(Business)
+admin.site.register(Village)
+admin.site.register(Profile)
